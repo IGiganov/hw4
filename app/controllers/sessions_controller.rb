6f9b2b0704fca6511412940a606b8e6af21833ae
IVAN GIGANOV <ivan.giganov@kellogg.northwestern.edu>
2023-03-08 23:10:17 +0000

    hw4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"username" => params["username"]})
+    
+    if @user
+      
+      if params["password"] == @user["password"]
+        redirect_to "/places"
+      else
+        redirect_to "/sessions/new"
+      end
+    
+    else
+      redirect_to "/sessions/new"
+    end
+
   end
 
   def destroy
   end
+
 end
   
